7eb1221fdeab65e8aeec6e674a7627789dcbe61b
kseniaglivko <k.glivko@mail.ru>
2021-07-29 20:41:54 +0300

    Создан первый вариант кода для пользовательских исключений.

--- exceptions.py
+++ exceptions.py
@@ -1,16 +1,33 @@
-"""
-Модуль, в котором создаются пользовательские классы исключений:
-InputParameterVerificationError и ResultVerificationError.
-"""
+"""Модуль, в котором создаются пользовательские классы исключений."""
 
 
 class InputParameterVerificationError(Exception):
-    """Исключение, выдаваемое в случае провала проверки входныхз параметров."""
+    """Исключение, возникающее в случае провала проверки входных параметров."""
 
-    pass
+    def __init__(
+        self,
+        input_value,
+        message="Данный параметр не прошел валидацию. Проверьте входные параметры.",
+    ):
+        self.input_value = input_value
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.input_value}: {self.message}"
 
 
 class ResultVerificationError(Exception):
-    """Исключение, выдаваемое в случае провала проверки входныхз параметров."""
+    """Исключение, возникающее в случае провала проверки выходных параметров."""
 
-    pass
+    def __init__(
+        self,
+        output_value,
+        message="Данный результат выполнения функции не прошёл валидацию.",
+    ):
+        self.output_value = output_value
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.output_value} - {self.message}"
