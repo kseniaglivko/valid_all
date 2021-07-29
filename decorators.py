7e18b587d9d414bb79b7d2c6459a3229d2ce50e9
kseniaglivko <k.glivko@mail.ru>
2021-07-29 23:39:27 +0300

    Отформатирован код, добавлены докстринги.

--- decorators.py
+++ decorators.py
@@ -4,26 +4,37 @@
 from exceptions import InputParameterVerificationError, ResultVerificationError
 
 
-# Необходимо реализовать функцию, которая бы извлекала данные для этого декоратора.
+# Необходимо реализовать функцию, которая бы извлекала данные для этой функции.
 def type_validator(type_):
+    """Функция проверяет входные или выходные данные на соответсвие требуемому типу."""
+
     def predicate(arg):
         return isinstance(arg, type_)
+
     return predicate
 
 
 # Этот декоратор должен обрабатывать входные данные.
 def input_validator(predicate):
+    """Валидатор типа входных данных. В качестве аргумента (predicate) должна передаваться функция "type_validator()",
+    через которую в декоратор и передается инофрмация о валидности типа данных для дальнейшей обработки."""
+
     def wrapper(function):
         def inner(arg):
             if not predicate(arg):
                 raise InputParameterVerificationError
             return function(arg)
+
         return inner
+
     return wrapper
 
 
 # Этот декоратор должен обрабатывать результат функции. Надо подумать, как их связать воедино.
 def output_validator(predicate, on_fail_repeat_times=1, default_behavior=None):
+    """Валидатор типа выходных данных. В качестве аргумента (predicate) должна передаваться функция "type_validator()",
+    через которую в декоратор и передается инофрмация о валидности типа данных для дальнейшей обработки."""
+
     def wrapper(function):
         def inner(arg):
             if not predicate(arg):
@@ -32,20 +43,27 @@ def output_validator(predicate, on_fail_repeat_times=1, default_behavior=None):
                         while on_fail_repeat_times > 0:
                             behaviour = default_behavior
                             counter = on_fail_repeat_times - 1
-                            output_validator(predicate, counter, default_behavior=behaviour)
+                            output_validator(
+                                predicate, counter, default_behavior=behaviour
+                            )
                             # А как проверяемая функция будет меняться?
                     if on_fail_repeat_times < 0:
                         behaviour = default_behavior
                         value = on_fail_repeat_times
-                        output_validator(predicate, on_fail_repeat_times=value, default_behavior=behaviour)
+                        output_validator(
+                            predicate,
+                            on_fail_repeat_times=value,
+                            default_behavior=behaviour,
+                        )
                 if default_behavior is not None:
                     default_behaviour()
                 raise ResultVerificationError
             return function(arg)
+
         return inner
+
     return wrapper
 
 
 def default_behaviour():
     print("¯\_(ツ)_/¯\nВсе плохо, переделывай!")
-
