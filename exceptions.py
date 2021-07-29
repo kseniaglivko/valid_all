ef6ef1927ecd76d39e05761f75e035d361311624
kseniaglivko <k.glivko@mail.ru>
2021-07-29 20:09:00 +0300

    Созданы шаблоны модулей для создания исключений и декоратора.

---
+++ exceptions.py
@@ -0,0 +1,16 @@
+"""
+Модуль, в котором создаются пользовательские классы исключений:
+InputParameterVerificationError и ResultVerificationError.
+"""
+
+
+class InputParameterVerificationError(Exception):
+    """Исключение, выдаваемое в случае провала проверки входныхз параметров."""
+
+    pass
+
+
+class ResultVerificationError(Exception):
+    """Исключение, выдаваемое в случае провала проверки входныхз параметров."""
+
+    pass
