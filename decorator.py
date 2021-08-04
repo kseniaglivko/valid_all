cbc4f512643decec735db99ca8f017223c3a0d43
kseniaglivko <k.glivko@mail.ru>
2021-08-04 22:33:16 +0300

    Приведен в порядок декоратор. Функцию ожидает дольнейшая доработка.

--- decorator.py
+++ decorator.py
@@ -1,52 +1,23 @@
-"""Модуль, в котором создается декораторы, для валидации входных и выходных параметров функции, а также типов данных
-ввода и вывода."""
-
-from exceptions import InputParameterVerificationError, ResultVerificationError
+"""Модуль, в котором создается декоратор для валидации входных и выходных параметров функции,"""
 
 
-# Примерная структура:
-# 1. Валидация json = V
-# 2. Валидация регулярных выражений = V
-# 3. Валидатор типов - начат ниже.
-# Необходимо реализовать функцию, которая бы извлекала данные для этой функции.
-def validate_type(type_):
-    """Функция проверяет входные или выходные данные на соответсвие требуемому типу."""
-
-    def predicate(arg):
-        return isinstance(arg, type_)
-
-    return predicate
+from typing import Any, Callable
 
 
-# Этот декоратор должен обрабатывать входные данные.
-def input_validator(predicate, on_fail_repeat_times=1, default_behavior=None):
+def valid_all(
+    precondition: Callable,
+    postcondition: Callable,
+    on_fail_repeat_times: int = 1,
+    default_behavior: Callable = None,
+) -> Callable:
     """
-    Валидатор входных данных. В качестве аргумента (predicate) должна передаваться одна из валидирующих функций,
-    выбираемых исходя из требуемого типа входных данных.
-    Через нее в valid_all передается инофрмация о валидности входных данных для дальнейшей обработки.
+    Валидатор выходных и выходных параметров функции.
+    В качестве аргумента precondition должна функция, отвечающая за валидацию входного файла и входных параметров.
+    В качестве аргумента postcondition должна передаваться функция, отвечающая за валидацию выходных параметров.
     """
 
-    def wrapper(function):
-        def inner(*args):
-            if not predicate(*args):
-                raise InputParameterVerificationError
-            return function(*args)
-
-        return inner
-
-    return wrapper
-
-
-# Этот декоратор должен обрабатывать результат функции.
-def output_validator(predicate, on_fail_repeat_times=1, default_behavior=None):
-    """
-    Валидатор выходных данных. В качестве аргумента (predicate) должна передаваться одна из валидирующих функций,
-    выбираемых исходя из требуемого типа входных данных.
-    Через нее в valid_all передается инофрмация о валидности выходных данных для дальнейшей обработки.
-    """
-
-    def wrapper(function):
-        def inner(*args):
+    def outer_wrapper(function: Callable) -> Callable:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             if not predicate(*args):
                 if on_fail_repeat_times != 0:
                     if on_fail_repeat_times > 0:
@@ -64,15 +35,3 @@ def output_validator(predicate, on_fail_repeat_times=1, default_behavior=None):
         return inner
 
     return wrapper
-
-
-@input_validator
-@output_validator(predicate, on_fail_repeat_times=1, default_behavior=None)
-def valid_all(function):
-    """Финальный декоратор, через применение которого будет реализовываться проверка требуемой функции."""
-    pass
-
-
-def default_behaviour():
-    print("¯\_(ツ)_/¯\nВероятно, этот нехороший человек наврал о своих способностях!")
-
